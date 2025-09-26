229b6ca64ccf98aebeaed52ee9e3e8aa9df242de
BlazeDrake <blaze.drake@outlook.com>
2025-09-26 10:23:34 -0600

    Finished orderRouter tests

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../service');
 const { Role, DB } = require('../database/database.js');
-const { randomName } = require('./testUtil.js');
+const { randomName, createAdminUser } = require('./testUtil.js');
 
 const testUserBase = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let registeredUser;
@@ -27,6 +27,37 @@ test('getMenu', async()=>{
     expect(getMenuRes.status).toBe(200);
 
     expect(getMenuRes.body).toStrictEqual(expected);
+})
+
+test('addMenuOptionValid', async()=>{
+    let baseUserInfo = await createAdminUser();
+    let loginRes= await request(app).put('/api/auth').send(baseUserInfo);
+    let adminToken = loginRes.body.token;
+
+    const itemName = randomName();
+    let itemToAdd = {
+        title: itemName,
+        description: "test",
+        price: 0.99,
+        image: "test"
+    }
+
+    let oldMenu = (await DB.getMenu());
+
+    let createMenuRes = await request(app).put('/api/order/menu/').set('authorization',`Bearer ${adminToken}`).send(itemToAdd);
+
+    expect(createMenuRes.status).toBe(200)
+    
+    let newMenu = await DB.getMenu();
+
+    expect(newMenu.length).toBe(oldMenu.length+1);
+    const hasItem = newMenu.some(item => item.title === itemName)
+    expect(hasItem).toBe(true)
+})
+
+test('addMenuOptionInvalid', async()=>{
+    let createMenuRes = await request(app).put('/api/order/menu/').set('authorization',`Bearer ${testUserAuthToken}`).send();
+    expect(createMenuRes.status).toBe(403);
 })
 
 test('createOrder',async()=>{
